3927e839b6ff8b8b804ee11b97977e6a73105e9f
ioppermann <ingo.oppermann@gmail.com>
2014-08-19 22:12:20 +0200

    Remove strlcpy in favor to strncpy

--- cronsh.c
+++ cronsh.c
@@ -120,37 +120,6 @@ int clock_gettime(int clk_id, struct timespec* t) {
 
 #ifdef __linux__
 /*
- * Copy src to string dst of size siz.  At most siz-1 characters
- * will be copied.  Always NUL terminates (unless siz == 0).
- * Returns strlen(src); if retval >= siz, truncation occurred.
- */
-size_t
-strlcpy(char *dst, const char *src, size_t siz)
-{
-	char *d = dst;
-	const char *s = src;
-	size_t n = siz;
-
-	/* Copy as many bytes as will fit */
-	if (n != 0) {
-		while (--n != 0) {
-			if ((*d++ = *s++) == '\0')
-				break;
-		}
-	}
-
-	/* Not enough room in dst, add NUL and traverse rest of src */
-	if (n == 0) {
-		if (siz != 0)
-			*d = '\0';		/* NUL-terminate dst */
-		while (*s++)
-			;
-	}
-
-	return(s - src - 1);	/* count does not include NUL */
-}
-
-/*
  * Appends src to string dst of size siz (unlike strncat, siz is the
  * full size of dst, not space left).  At most siz-1 characters
  * will be copied.  Always NUL terminates (unless siz <= strlen(dst)).
@@ -699,10 +668,10 @@ void cronsh_init(void) {
 
 	env = getenv("CRONSH_HOSTNAME");
 	if(env != NULL) {
-		strlcpy(config.thishostname, env, sizeof(config.thishostname));
+		strncpy(config.thishostname, env, sizeof(config.thishostname));
 	}
 	else if(gethostname(config.thishostname, sizeof(config.thishostname)) != 0) {
-		strlcpy(config.thishostname, "[unknown]", sizeof(config.thishostname));
+		strncpy(config.thishostname, "[unknown]", sizeof(config.thishostname));
 	}
 
 	config.thishostname[sizeof(config.thishostname) - 1] = '\0';
@@ -714,15 +683,15 @@ void cronsh_init(void) {
 	
 	env = getenv("USER");
 	if(env != NULL) {
-		strlcpy(config.thisuser, env, sizeof(config.thisuser));
+		strncpy(config.thisuser, env, sizeof(config.thisuser));
 	}
 	else {
 		env = getenv("LOGNAME");
 		if(env != NULL) {
-			strlcpy(config.thisuser, env, sizeof(config.thisuser));
+			strncpy(config.thisuser, env, sizeof(config.thisuser));
 		}
 		else {
-			strlcpy(config.thisuser, "[unknown]", sizeof(config.thisuser));
+			strncpy(config.thisuser, "[unknown]", sizeof(config.thisuser));
 		}
 	}
 
