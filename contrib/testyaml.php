91412977f0ed1143da249ac5d9654c1352f4ce1c
ioppermann <ingo.oppermann@gmail.com>
2016-06-11 16:15:35 +0200

    Add line numbers to yams output
    The error message shows the line number that contains the error

--- contrib/testyaml.php
+++ contrib/testyaml.php
@@ -1,6 +1,13 @@
 #!/opt/local/bin/php56
 <?php
 	$input = stream_get_contents(STDIN);
+	$line = explode("\n", $input);
+
+	$i = 0;
+	foreach($line as $l) {
+		printf("%4d: %s\n", $i, $l);
+		$i++;
+	}
 
 	$data = yaml_parse($input);
 
